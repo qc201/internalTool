08dadb852822e857f8ce90eb6ac0f7e4179191b3
Qiuyu Chen <84677030+qc201@users.noreply.github.com>
2022-03-11 07:41:18 -0800

    add connection to mongodb

--- app.js
+++ app.js
@@ -1,46 +1,80 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/topicDB");
-const itemsSchema = { name: String };
-const Item = mongoose.model("Item", itemsSchema);
 
+const homeStartingContent = "The lake or the easy-to- Mattis annoys a targeted at it was not a part of the body. The laughter of the fringe at any one, but the hatred of the superhero.";
+const information = "There will always be laughter in the planning of the pregnant makeup of the individual who does not have the earth or the average person";
+const contactContent = "Scelerisque eleifend dum pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Sometimes the pain is to be put to the Internet itself, the pain is to be loved to the main storage space. and that the consequences should always be monitored for free.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('home')
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 });
 
-app.get('/new', (req, res) => {
-    res.render('new')
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
 
 
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
 
+app.get("/posts/:postId", function(req, res){
 
+const requestedPostId = req.params.postId;
 
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
 
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
-
-
-
-
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
